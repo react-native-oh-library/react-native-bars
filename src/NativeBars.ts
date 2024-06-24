be9a1872570b00aed52d5cadf453f6154a26103f
wjy <18071094885@163.com>
2024-06-24 14:24:32 +0800

    refactor: 删除harmony无关代码

--- src/NativeBars.ts
+++
@@ -1,9 +0,0 @@
-import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
-import { TurboModuleRegistry } from 'react-native';
-
-export interface Spec extends TurboModule {
-    setStatusBar: () => void;
-    setNavigationBar: () => void;
-} 
- 
-export default TurboModuleRegistry.get<Spec>('BarsNativeModule') as Spec | null;
