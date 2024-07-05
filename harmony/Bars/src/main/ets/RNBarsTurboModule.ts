052f0929e7ab637044e2068658eb486a29950b10
wjy <18071094885@163.com>
2024-07-05 17:45:59 +0800

    fix: 修改harmony适配用turboModule实现状态栏控制

---
+++ harmony/Bars/src/main/ets/RNBarsTurboModule.ts
@@ -0,0 +1,80 @@
+/**
+ * MIT License
+ *
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { TurboModule, TurboModuleContext } from '@rnoh/react-native-openharmony/ts';
+import { TM } from '@rnoh/react-native-openharmony/generated/ts';
+import window from '@ohos.window';
+import Logger from './Logger'
+
+export class RNBarsTurboModule extends TurboModule implements TM.BarsNativeModule.Spec {
+  isKeepScreenOn: boolean = true;
+  unisKeepScreenOn: boolean = false;
+
+  constructor(ctx) {
+    super(ctx);
+  }
+
+  //颜色取值逻辑
+  getBarColorByType (barStyle): string {
+    if(barStyle == 'light-content'){
+      barStyle =  '#ffffffff';
+    }else if(barStyle == 'dark-content'){
+      barStyle =  '#ff000000';
+    }
+    return  barStyle;
+  }
+
+  public setStatusBarStyle(barStyle): void {
+    try {
+      window.getLastWindow(getContext(this), (err, w) => {
+        console.info('setStatusBar666'+barStyle)
+        w.setWindowSystemBarProperties({
+          // 状态栏颜色
+          //   statusBarColor: '#66666666',
+          // 状态栏文字颜色
+          statusBarContentColor: this.getBarColorByType(barStyle),
+        })
+      })
+    } catch (exception) {
+      Logger.error(exception);
+    }
+  }
+
+  public setNavigationBarStyle(barStyle): void {
+    try {
+      window.getLastWindow(getContext(this), (err, w) => {
+        w.setWindowSystemBarProperties({
+          // 导航栏颜色
+          //   navigationBarColor:'#ff000000',
+          // 导航栏文字颜色
+          navigationBarContentColor: this.getBarColorByType(barStyle),
+        })
+      })
+    } catch (exception) {
+      Logger.error(exception);
+    }
+  }
+
+
+}
--- src/StatusBar.ts
+++
@@ -1,8 +0,0 @@
-import type { ViewProps } from "react-native/Libraries/Components/View/ViewPropTypes";
-import type { HostComponent } from "react-native";
-import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
-export interface NativeProps extends ViewProps {
-    barStyle?: string;
- // 添加其它 props
-}
-export default codegenNativeComponent<NativeProps>("NativeSampleView") as HostComponent<NativeProps>;
